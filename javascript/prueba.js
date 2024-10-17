34aad1eae2181fea11296f8802d7f56d26203c4f
Damian Alvarez <damianalva20044@gmail.com>
2024-10-17 12:33:25 -0600

    cambio

---
+++ javascript/prueba.js
@@ -0,0 +1,13 @@
+//
+alert("NO toques el boton rojo")
+function saludo(){
+    alert("que te pasa estupido")
+}
+function hola(){
+    document.getElementById("container").style.backgroundColor="Blue"
+
+}
+function cambio(){
+    if(color==("rgb(0,0,255)")){
+        alert(color)}
+}
