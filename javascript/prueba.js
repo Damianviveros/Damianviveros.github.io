397647bbeed336f856767216f108ccdbbb6350ce
Damian Alvarez <damianalva20044@gmail.com>
2024-10-19 20:27:28 -0600

    mayor avance

--- javascript/prueba.js
+++ javascript/prueba.js
@@ -1,13 +1,3 @@
-//
-alert("NO toques el boton rojo")
-function saludo(){
-    alert("que te pasa estupido")
-}
-function hola(){
-    document.getElementById("container").style.backgroundColor="Blue"
-
-}
 function cambio(){
-    if(color==("rgb(0,0,255)")){
-        alert(color)}
+    document.getElementById('imagen').style.height='40px';
 }
